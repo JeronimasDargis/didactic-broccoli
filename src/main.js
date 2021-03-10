8666f0d6268f531fe515a8678b3e8b7b82ff8dc7
Jeronimas Dargis <jeronimasd@gmail.com>
2021-03-10 10:02:44 +0200

    feat: working on implementing test component and publishing to npm

--- src/main.js
+++ src/main.js
@@ -1,4 +1,34 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import * as Components from "./components";
 
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+
+const removeKeysFromObject = (object, keys) => {
+  return Object.entries(object).reduce((obj, [key, value]) => {
+    if (!keys.includes(key) && !keys.includes(value.name)) {
+      obj[key] = value;
+    }
+    return obj;
+  }, {});
+};
+
+const library = {
+  install(Vue, options) {
+    let pluginComponents = Components;
+
+    const toRemove = options && options.remove ? options.remove : null;
+    if (toRemove && Array.isArray(toRemove)) {
+      pluginComponents = removeKeysFromObject(Components, toRemove);
+    }
+
+    for (let plugin in pluginComponents) {
+      Vue.component(plugin, Components[plugin]);
+    }
+  },
+};
+
+createApp(App).mount("#app");
+
+export default library;
+
+export * from "./components";
